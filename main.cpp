c06f832b9a3c00f835759be93813e0bdd6bc5d95
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-04-06 16:39:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     cin >> n;
     primo = 1;
     secondo =0;
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < n+1 ; i++)
     {
         somma = primo + secondo;
         if(somma<=n)
